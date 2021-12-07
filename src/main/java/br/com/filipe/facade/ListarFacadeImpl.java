6e80bbb440b139667867eb8d87ee6128a1c80163
AraujoFilipeRosa <araujo.filiperosa@gmail.com>
2021-12-07 16:51:54 -0300

    Quarto commit

--- src/main/java/br/com/filipe/facade/ListarFacadeImpl.java
+++ src/main/java/br/com/filipe/facade/ListarFacadeImpl.java
@@ -1,16 +1,11 @@
 package br.com.filipe.facade;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.filipe.dao.ListarDao;
 import br.com.filipe.dao.ListarDaoImpl;
 
-@Service
 public class ListarFacadeImpl implements ListarFacade {
 
-	@Autowired
-	private ListarDao listarDaoImpl;
+	private ListarDao listarDaoImpl = new ListarDaoImpl();
 	
 	@Override
 	public void listar() throws Exception {
